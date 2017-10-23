8907e0c049ab905226159b7b9598d64cb6c50e1b
Tyler Kadow <tylerkadow@aol.com>
2017-10-23 18:11:51 -0400

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root1",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host : "localhost",
+    user: "root",
+    password: "root1",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
